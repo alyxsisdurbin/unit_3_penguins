a60640adac97444ce18b272f5c90c418e120932f
alyxsisdurbin <hdurbin@email.sc.edu>
2023-02-28 11:23:05 -0500

    notes

--- class notes/class_notes_2023-02-28.R
+++ class notes/class_notes_2023-02-28.R
@@ -43,4 +43,40 @@ my_results=broom::tidy(lm_3, conf.int=TRUE) %>% #how to add confidence intervals
 
 # Visualize model
 
-ggPredict(lm_3) #from the giraffe package
+ggPredict(lm_3, se=TRUE, interactive=TRUE) #from the giraffe package, se=TRUE makes there a dim standard error model
+
+lm_3_predictions=predict(lm_3, interval="confidence", level=0.95)
+head(lm_3_predictions)
+
+penguins_lm_3_predict = cbind(penguins_lm_3, lm_3_predictions)
+head(penguins_lm_3_predict)
+
+ggplot(data=penguins_lm_3_predict, aes(x=bill_length_mm, y=bill_depth_mm, color=species)) +
+  geom_ribbon(aes(ymin=lwr, ymax=upr, fill=species, color=NULL), alpha=0.5) +
+  geom_point() +
+       geom_line(aes(y=fit)) 
+
+# generate new data
+newdata_bill_length_mm = seq(min(penguins_lm_3$bill_length_mm), 
+                             max(penguins_lm_3$bill_length_mm),
+                             by=0.1)
+head(newdata_bill_length_mm)       
+
+newdata = expand.grid(bill_length_mm=newdata_bill_length_mm, 
+                      species=unique(penguins_lm_3$species))
+newdata_predict_lm_3 = predict(lm_3, newdata=newdata)
+
+newdata_predict_lm_3 = cbind(newdata, predict(lm_3, newdata=newdata, interval="confidence"))
+
+ggplot()+
+  geom_point(data=penguins_lm_3, aes(x=bill_length_mm, y=bill_depth_mm, color=species))+
+  geom_ribbon(data=newdata+predict_lm_3, aes(ymin=lwr, ymax=upr, x=bill_length_mm, y=bill_depth_mm, fill=species, alpha=0.5))+
+  geom_line()
+
+#tidyverse way of generating predictions
+lm_3_predict = lm_3 %>%
+  broom::augment(data=penguins_lm_3, se_fit=TRUE, interval="confidence")
+glimpse(lm_3_predict)
+
+
+                      
